b9962596feb8b015e1b4c337a4eedae106fdbd6c
Kamycz3q <overek.email@gmail.com>
2023-05-08 21:49:48 +0200

    first commit

---
+++ src/main/java/com/kamycz3q/rentalspringboot/RentalSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.kamycz3q.rentalspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kamycz3q/rentalspringboot/RentalSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamycz3q.rentalspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
