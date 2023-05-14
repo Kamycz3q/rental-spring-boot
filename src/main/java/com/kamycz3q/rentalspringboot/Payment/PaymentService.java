7118dd6c3f35f34d2e17ac6c8755de1d9036b174
Maciej Posłuszny <xmachiei@gmail.com>
2023-05-14 12:23:58 +0200

    Dodano implementację PaymentService

--- src/main/java/com/kamycz3q/rentalspringboot/Payment/PaymentService.java
+++ src/main/java/com/kamycz3q/rentalspringboot/Payment/PaymentService.java
@@ -1,5 +1,11 @@
 package com.kamycz3q.rentalspringboot.Payment;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PaymentService {
+
+    public void pay(Payment payment) {
     
+    }
 }
